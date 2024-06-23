bd0539b72ae87df66e94b972cacc2fc0166e1c9b
Anukarsh <anukarshr@gmail.com>
2024-06-23 17:40:15 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-let random = math.random()
+let random = Math.random()
+let a = prompt("Enter first number")
+let b = prompt("Enter second number")
+let c = prompt("Enter operation")
+
+let obj = {
+    "+":"-",
+    "*":"+",
+    "-":"/",
+    "/":"**"
+}
 
 if(random>0.1){
-    
+    // perform correct calculation
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
+else{
+    // perform incorrect calculation
+    c=obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
 }
